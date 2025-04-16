7ec4edae46cde6a27b58b48c5a3ef6d5538c0642
CharlesChi <qiyaofu715@gmail.com>
2025-04-16 16:34:36 +1000

    Apply forms in log in and register page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../services/AuthService';
-import Navbar from './Navbar';
+import Form from './Form';
 
 function Login() {
   const navigate = useNavigate();
@@ -14,10 +14,9 @@ function Login() {
     try {
       const response = await loginUser(email, password);
       localStorage.setItem('authData', JSON.stringify(response));
-      // Clear input fields on successful login
       setEmail('');
       setPassword('');
-      setError(''); // Optional: Clear error message as well
+      setError('');
       navigate('/dashboard');
     } catch (err) {
       setError(err.message || 'Login failed. Please check your email or password.');
@@ -25,49 +24,15 @@ function Login() {
   };
 
   return (
-    <>      
-      <div className=" bg-bigbrain-light-mint flex justify-center items-center min-h-screen ">
-        <form 
-          onSubmit={handleLoginSubmit}
-          className="p-6 bg-bigbrain-milky-white rounded shadow-md"
-          autoComplete="off"
-        >
-          
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Email"
-              required
-              className="block w-full p-2 border rounded"
-              autoComplete="off"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              required
-              className="block w-full p-2 border rounded bg-bigbrain-milky-white"
-              autoComplete="off"
-            />
-          </div>
-          {error && <p className="text-red-500 mb-4">{error}</p>}
-          <button type="submit" className="bg-bigbrain-light-pink font-bold w-full p-2 text-white rounded hover:bg-bigbrain-dark-pink hover:cursor-pointer">
-            Log in
-          </button>
-        </form>
-      </div>
-    </>
+    <Form
+      onSubmit={handleLoginSubmit}
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      error={error}
+      buttonText="Log in"
+    />
   );
 }
 
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,5 +1,48 @@
-export default function Register() {
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { apiCall } from '../utils/api';
+import Form from './Form';
+
+function Register() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [name, setName] = useState('');
+  const handleRegisterSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+    try {
+      const response = await apiCall('/admin/auth/register', 'POST', { email, password, name });
+      setName('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+      setError('');
+      navigate('/login');
+    } catch (err) {
+      setError(err.message || 'Registration failed. Please try again.');
+    }
+  };
   return (
-    <div>Register</div>
-  )
+    <Form
+      onSubmit={handleRegisterSubmit}
+      name={name}
+      setName={setName}
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      confirmPassword={confirmPassword}
+      setConfirmPassword={setConfirmPassword}
+      error={error}
+      buttonText="Register"
+    />
+  );
 }
+
+export default Register;
