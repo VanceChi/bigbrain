99d7c63bf8344fec022bcdc84f4d50e2e3ac0148
CharlesChi <qiyaofu715@gmail.com>
2025-04-17 13:04:05 +1000

    Move routers into pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
 import Page from './pages/Page'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Page pageName='Login' />} />
-        <Route path="/login" element={<Page pageName='Login' />} />
-        <Route path="/register" element={<Page pageName='Register' />} />
-        <Route path="/dashboard" element={<Page pageName='Dashboard' />} />
-      </Routes>
-    </Router>
+    <>
+      <Page />
+    </>
+    
   );
 }
 
--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -1,18 +1,33 @@
-import Navbar from "../components/Navbar"
 import Register from "./Register"
 import Login from "./Login"
 import Dashboard from "./Dashboard"
-export default function Page({pageName}) {
-  const pages = {
-    'Register': <Register />,
-    'Login': <Login />,
-    'Dashboard': <Dashboard />
-  }
+import EditGame from "./EditGame";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
+export default function Page({pageName}) {
+  // const pages = {
+  //   'Register': <Register />,
+  //   'Login': <Login />,
+  //   'Dashboard': <Dashboard />
+  // }
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    setToken(JSON.parse(localStorage.getItem('authData')).token)
+  }, []);
+  
   return (
     <>
-      <Navbar pageName={pageName}/>
-      {pages[pageName] || <div>Page not found</div>}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/game/:gameId" element={<EditGame />} />
+        </Routes>
+      </Router>
+      {/* {pages[pageName] || <div>Page not found</div>} */}
     </>
   )
 }
