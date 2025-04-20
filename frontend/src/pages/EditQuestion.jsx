af4506d17b0146c07905446a30695602dc99e5f1
CharlesChi <qiyaofu715@gmail.com>
2025-04-20 17:35:50 +1000

    Build EditQuestion page

--- frontend/src/pages/EditQuestion.jsx
+++ frontend/src/pages/EditQuestion.jsx
@@ -1,14 +1,38 @@
 import Navbar from "../components/Navbar"
 import { BackButton } from "../components/Button"
+import { useParams } from "react-router-dom"
+import { queryGame, queryQuestions, queryQuestion } from "../utils/query";
+import { useEffect, useState } from "react";
+import EditQuizQuestionCard from "../components/QuestionEditor";
 
 export default function EditQuestion () {
-
+  const { gameId, questionId } = useParams();
+  const [questions, setQuestions] = useState();
   
+  useEffect(() => {
+    (async () => {
+      try {
+        const questions = await queryQuestions(gameId);
+        setQuestions(questions)
+      } catch (err) {
+        console.error("Failed to load games:", err);
+      }
+    })();
+    
+  }, []);
+
+
   return (
     <>
       <Navbar />
       <BackButton />
-      <p>EditQuestion</p>
+      <EditQuizQuestionCard  
+        gameId={gameId}
+        questionId={questionId}
+        showAddQues={true}
+        questions={questions}
+        setQuestions={setQuestions}
+      />
     </>
     
   )
