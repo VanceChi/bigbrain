8b1f422d313bdf2de6f3e42599a45e7687938604
CharlesChi <qiyaofu715@gmail.com>
2025-04-24 17:56:43 +1000

    Built genAnsResult() function.

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,33 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+Results varibale in Session: (The results of games)
+[
+  {
+    "name":"b",
+    "answers":[
+      {
+        "questionStartedAt":"2025-04-24T04:35:41.710Z","answeredAt":"2025-04-24T04:35:44.194Z",
+        "answers":["Beijing"],
+        "correct":false
+      },
+      {
+        "questionStartedAt":null,
+        "answeredAt":null,
+        "answers":[],
+        "correct":false
+      },
+      {
+        "questionStartedAt":null,
+        "answeredAt":null,
+        "answers":[],
+        "correct":false
+      }
+    ]
+  },
+  
+  {"name":"a",
+  "answers":[{"questionStartedAt":"2025-04-24T04:35:41.710Z","answeredAt":"2025-04-24T04:35:46.252Z","answers":["Beijing","Shanghai"],"correct":true},{"questionStartedAt":null,"answeredAt":null,"answers":[],"correct":false},{"questionStartedAt":null,"answeredAt":null,"answers":[],"correct":false}]}
+  ]
--- frontend/src/pages/Session.jsx
+++ frontend/src/pages/Session.jsx
@@ -6,6 +6,7 @@ import { SessionContext } from "../context/Sessions";
 import { BackButton } from "../components/Button";
 import { apiCall } from "../utils/api";
 import { QuestionDisplay } from "../components/EditQuestionCard";
+import { queryQuestions } from "../utils/query";
 
 export default function Session() {
   const {activeSessions, setActiveSessions} = useContext(SessionContext);
@@ -17,6 +18,9 @@ export default function Session() {
   const [position, setPosition] = useState(-1);  // -1: not start yet
   const [question, setQuestion] = useState({}); 
   const [nofQuestion, setNofQuestion] = useState(0); 
+  const [showResult, setShowResult] = useState(false); 
+  const [resResults, setResResults] = useState(''); 
+  const [ansResults, setAnsResults] = useState([]); 
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,26 +42,83 @@ export default function Session() {
   // Since cant restart session. The effect here means active -> unactive
   // Shows result.
   useEffect(() => {
-    const loadResult = async () => {
-      const res = await apiCall(`/admin/session/${sessionId}/results`, 'GET'); //{results: Array(0)}
-      setResults(res.results);
-      debugger
-    }
-
     
     getStatus();
-    
   }, [active])
 
+  useEffect(() => {
+    if (showResult){
+      loadResult();
+    }
+      
+  }, [showResult])
+
+  /**
+   * Given response from 
+   * 
+   * @param {*} resResults 
+   */
+  const genAnswerResult = async (resResults) => {
+    const questions = await queryQuestions(gameId);
+    console.log('genAnswerResult----------')
+    console.log('questions:', questions);
+    console.log('resResults', resResults);
+    console.log(nofQuestion);
+    const ansResults = [];  
+    /**
+     * Three dimesion: question, player, [correct, time]
+     * [
+     *  [   q1
+     *    [correct, time] player1
+     *    [correct, time] player2
+     *  ],
+     *  [   q2
+     *    [correct, time] player1
+     *    [correct, time] player2
+     *  ]  
+     * ]
+     */
+    for (let i=0; i<nofQuestion; i++){  // question
+      const qArr = [];
+      const points = questions[i].points;
+      const questionText = questions[i].questionText;
+      for (let j=0; j<resResults.length; j++) {  // player
+        const p = resResults[j];
+        const correct = p.answers[i].correct;
+        const time = new Date(p.answers[i].answeredAt) - new Date(p.answers[i].questionStartedAt);
+        const playerName = p.name;
+        qArr.push({correct, time, points, playerName, questionText});
+      }
+      ansResults.push(qArr);
+    }
+    console.log('ansResults:', ansResults);
+    return ansResults;
+  }
   
+  const loadResult = async () => {
+    // console.log('loadResult()')
+    const res = await apiCall(`/admin/session/${sessionId}/results`, 'GET'); //{results: Array(0)}
+    const resResults = res.results;
+    setResResults(resResults);
+
+    // generate customize answering results
+    const ansResults = await genAnswerResult(resResults);
+    setAnsResults(ansResults);
+  }
+
   const getStatus = async () => {
     const res = await apiCall(`/admin/session/${sessionId}/status`, 'GET')
     const results = res.results;
     setNofQuestion(results.questions.length);
     const position = results.position;
     const question = results.questions[position]??{};
-    setPosition(position);
-    setQuestion(question);
+    if (Object.keys(question).length === 0) {
+      setShowResult(true);
+    } else {
+      setShowResult(false);
+      setPosition(position);
+      setQuestion(question);
+    }
   }
   
   const handleEndSession = async () => {
@@ -65,6 +126,7 @@ export default function Session() {
       const res = await endSession(undefined, sessionId, activeSessions, setActiveSessions);
       console.log('Endsession res:', res);
       setActive(false);
+      setShowResult(true);
     } catch (error) {
       console.log(error);
     }
@@ -86,7 +148,7 @@ export default function Session() {
           <button onClick={handleEndSession} className="border">End Session</button>
         </>
       ) : (
-        <p>Session unactive</p>
+        <p>Session inactive</p>
       )}
       <p>Session of {title} -- session id: {sessionId}</p>
       <button className="border inline-block" onClick={hanleAdvanceQuestion}>Advance Question</button> &nbsp;&nbsp;&nbsp;
@@ -95,15 +157,9 @@ export default function Session() {
         {(-1 < position && position < nofQuestion) && `${position+1} out of ${nofQuestion}`}
         {position === nofQuestion && 'Finished'}
       </p>
-      {/* <div className="border">
-        {Object.keys(question).length !== 0 ? (
-          <p>{JSON.stringify(question)}</p>
-        ) : (
-          <p>Not start yet</p>
-        )}
-      </div> */}
+
       <div className="border">
-        {Object.keys(question).length !== 0 ? (
+        {Object.keys(question).length !== 0 && !showResult && (
           <QuestionDisplay 
             questionType={question.questionType}
             questionText={question.questionText}
@@ -117,8 +173,10 @@ export default function Session() {
             setResult={()=>console.log('setResult.')}
             mode={'observe'}
           />
-        ) : (
-          <p>Not start yet</p>
+        )} 
+
+        {showResult && (
+          <p>results: {JSON.stringify(resResults)}</p>
         )}
       </div>
     </>
