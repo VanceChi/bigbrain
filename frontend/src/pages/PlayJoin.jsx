0485140a5ce03f1610a00b9f0f5f9765b66ea6ab
CharlesChi <qiyaofu715@gmail.com>
2025-04-25 18:40:58 +1000

    Improve play join UI

--- frontend/src/pages/PlayJoin.jsx
+++ frontend/src/pages/PlayJoin.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import { useState } from "react";
 import { BackButton } from "../components/Button";
 import { apiCall } from "../utils/api";
+import { BackBtn } from "../components/SVGBtn";
 
 export default function PlayJoin() {
   const { sessionId } = useParams();
@@ -11,8 +12,12 @@ export default function PlayJoin() {
   const [sessionIdSubmitted, setSessionIdSubmitted] = useState(sessionId!==undefined);
   const [name, setName] = useState('');
   const [sessionIdError, setSessionIdError] = useState(false);
+  const [inputError, setInputError] =useState('');
   
   const handleSubmitSessionId = async () => {
+    if (!inputSessionId)
+      setInputError('Input session Id!')
+    setInputError('');
     if(inputSessionId) {
       setSessionIdSubmitted(true);
       navigate(`/play/join/${inputSessionId}`)
@@ -20,39 +25,81 @@ export default function PlayJoin() {
   }
 
   const handleSubmitName = async () => {
+    if (!name) 
+      setInputError('Input name!')
+    setInputError('');
     try {
       const { playerId } = await apiCall(`/play/join/${sessionId}`, 'POST', {name});
       navigate(`/play/${playerId}`, { state: { sessionId } });
+      setInputError('');
     } catch (error) {
       console.error('Player join session error:', error);
-      setSessionIdError(true);
+      setInputError(error.message);
     }
     console.log('waiting.')
     }
     
   return (
     <>
-      <Navbar onClick={()=>navigate('/play/join')}/>
-      <BackButton /> <br />
-      <p>Join The Game</p>
-      { sessionIdSubmitted ? (
-        <>
-          <input type="text" value={name} placeholder="Enter Name" onChange={e => setName(e.target.value)}/>
-          <button onClick={handleSubmitName}>Submit Name</button>
-        </>
-      ) : (
-        <>
-          <input type="text" value={inputSessionId} placeholder="Enter Session id" onChange={e => setInputSessionId(e.target.value)}/>
-          <button onClick={handleSubmitSessionId}>Submit Session id</button>
-        </>
-      )}
-
-      {/* Session Id Error Display */}
-      {sessionIdError && (
-        <div aria-label="Error information container" role="alert">
-          <p>Session Id invalid!</p>
+      <Navbar/>
+      <BackBtn onClick={()=> {
+        navigate('/play/join');
+        setSessionIdSubmitted(false);
+      }}/>
+      <div className="bg-bigbrain-milky-canvas rounded-3xl shadow-2xl  h-[600px] p-10 m-5">
+        <div aria-label="Title" className="place-items-center">
+          <p className="text-2xl font-bold mb-4 italic">Join The Game</p>
         </div>
-      )}
+        <div className=" m-10flex place-items-center">
+          <div aria-label="input-session-id" className="flex place-content-center">
+            { sessionIdSubmitted ? (
+              <div className="m-10 mt-30">
+                <p className="text-[25px] font-bold mb-4 italic">Give your name here !</p>
+                <div aria-label="input-name" className="mt-10">
+                  <input 
+                    type="text" 
+                    value={name} 
+                    placeholder="Enter Name" 
+                    onChange={e => setName(e.target.value)}
+                    className="border-2 rounded-md mr-4 h-[50px]"
+                  />
+                  <button 
+                    onClick={handleSubmitName}
+                    className="border h-full p-3 rounded-2xl text-black font-bold hover:cursor-pointer hover:bg-bigbrain-light-pink hover:text-white"
+                  >Submit</button>
+                </div>
+              </div>
+            ) : (
+              <div aria-label="input-name" className="mt-30">
+                <p className="text-[25px] font-bold mb-4 italic">
+                  Give Session Id here !
+                </p>
+                <div aria-label="input-name" className="mt-10">
+                  <input 
+                    type="text" 
+                    value={inputSessionId} 
+                    placeholder="Enter Session id" 
+                    onChange={e => setInputSessionId(e.target.value)}
+                    className="border-2 rounded-md mr-4 h-[50px]"
+                  />
+                  <button 
+                      onClick={handleSubmitSessionId}
+                      className="border p-3 rounded-2xl text-black font-bold hover:cursor-pointer hover:bg-bigbrain-light-pink hover:text-white"
+                  >Submit
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* Session Id Error Display */}
+          {inputError && (
+            <div aria-label="Error information container" role="alert" className="m-10">
+              <p className="text-red-900 font-bold">{inputError}</p>
+            </div>
+          )}
+        </div>
+      </div>
       
     </>
   )
